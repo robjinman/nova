7c2f51267ffa3e39af77af1ecfc9fb796f67fe1e
Rob Jinman <jinmanr@gmail.com>
2025-01-26 18:05:43 +0000

    Descriptor set per texture

--- src/application.cpp
+++ src/application.cpp
@@ -1,6 +1,9 @@
 #include "application.hpp"
 #include "renderer.hpp"
 #include "logger.hpp"
+#include "exception.hpp"
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 #include <iostream>
 
 const uint32_t WIDTH = 800;
@@ -15,25 +18,31 @@ public:
   void run() override;
 };
 
+TexturePtr loadTexture(const std::string& filePath)
+{
+  int width = 0;
+  int height = 0;
+  int channels = 0;
+  stbi_uc* pixels = stbi_load(filePath.c_str(), &width, &height, &channels, STBI_rgb_alpha);
+
+  if (!pixels) {
+    EXCEPTION("Failed to load texture image");
+  }
+
+  TexturePtr texture = std::make_unique<Texture>();
+  texture->width = width;
+  texture->height = height;
+  texture->channels = channels;
+  texture->data.resize(width * height * channels);
+  memcpy(texture->data.data(), pixels, width * height * channels);
+
+  stbi_image_free(pixels);
+
+  return texture;
+}
+
 void ApplicationImpl::run()
 {
-  ModelPtr model = std::make_unique<Model>();
-  model->vertices = {
-    {{ -0.5f, -0.5f, 0.0f }, { 1.0f, 0.0f, 0.0f }, { 1.0f, 0.0f }},
-    {{ 0.5f, -0.5f, 0.0f }, { 0.0f, 1.0f, 0.0f }, { 0.0f, 0.0f }},
-    {{ 0.5f, 0.5f, 0.0f }, { 0.0f, 0.0f, 1.0f }, { 0.0f, 1.0f }},
-    {{ -0.5f, 0.5f, 0.0f }, { 1.0f, 1.0f, 1.0f }, { 1.0f, 1.0f }},
-
-    {{ -0.5f, -0.5f, -0.5f }, { 1.0f, 0.0f, 0.0f }, { 1.0f, 0.0f }},
-    {{ 0.5f, -0.5f, -0.5f }, { 0.0f, 1.0f, 0.0f }, { 0.0f, 0.0f }},
-    {{ 0.5f, 0.5f, -0.5f }, { 0.0f, 0.0f, 1.0f }, { 0.0f, 1.0f }},
-    {{ -0.5f, 0.5f, -0.5f }, { 1.0f, 1.0f, 1.0f }, { 1.0f, 1.0f }}
-  };
-  model->indices = {
-    0, 1, 2, 2, 3, 0,
-    4, 5, 6, 6, 7, 4
-  };
-
   glfwInit();
 
   glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // Don't create OpenGL context
@@ -45,6 +54,27 @@ void ApplicationImpl::run()
 
   {
     auto renderer = CreateRenderer(*window, *logger);
+
+    auto texture = loadTexture("textures/texture.png");
+    auto textureId = renderer->addTexture(std::move(texture));
+
+    ModelPtr model = std::make_unique<Model>();
+    model->vertices = {
+      {{ -0.5f, -0.5f, 0.0f }, { 1.0f, 0.0f, 0.0f }, { 1.0f, 0.0f }},
+      {{ 0.5f, -0.5f, 0.0f }, { 0.0f, 1.0f, 0.0f }, { 0.0f, 0.0f }},
+      {{ 0.5f, 0.5f, 0.0f }, { 0.0f, 0.0f, 1.0f }, { 0.0f, 1.0f }},
+      {{ -0.5f, 0.5f, 0.0f }, { 1.0f, 1.0f, 1.0f }, { 1.0f, 1.0f }},
+
+      {{ -0.5f, -0.5f, -0.5f }, { 1.0f, 0.0f, 0.0f }, { 1.0f, 0.0f }},
+      {{ 0.5f, -0.5f, -0.5f }, { 0.0f, 1.0f, 0.0f }, { 0.0f, 0.0f }},
+      {{ 0.5f, 0.5f, -0.5f }, { 0.0f, 0.0f, 1.0f }, { 0.0f, 1.0f }},
+      {{ -0.5f, 0.5f, -0.5f }, { 1.0f, 1.0f, 1.0f }, { 1.0f, 1.0f }}
+    };
+    model->indices = {
+      0, 1, 2, 2, 3, 0,
+      4, 5, 6, 6, 7, 4
+    };
+    model->texture = textureId;
 
     renderer->addModel(std::move(model));
 
