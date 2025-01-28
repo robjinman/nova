58e3e6d808825856c56e2d7aafc6190cf917ddf8
Rob Jinman <jinmanr@gmail.com>
2025-01-28 23:23:07 +0000

    Implement FPS controls

--- src/game.cpp
+++ src/game.cpp
@@ -2,10 +2,15 @@
 #include "renderer.hpp"
 #include "time.hpp"
 #include "camera.hpp"
+#include "logger.hpp"
+#include "utils.hpp"
 #include <set>
 
 namespace
 {
+
+const double MOUSE_LOOK_SPEED = 2.5;
+const float_t PLAYER_SPEED = 4.f;
 
 class GameImpl : public Game
 {
@@ -14,16 +19,22 @@ class GameImpl : public Game
 
     void onKeyDown(KeyboardKey key) override;
     void onKeyUp(KeyboardKey key) override;
-    void update(const Vec2f& mouseDelta) override;
+    void onMouseMove(const Vec2f& delta) override;
+    void update() override;
 
   private:
     Logger& m_logger;
     Renderer& m_renderer;
     Camera m_camera;
     std::set<KeyboardKey> m_keysPressed;
+    Vec2f m_mouseDelta;
     Timer m_timer;
     ModelId m_model1Id;
     ModelId m_model2Id;
+
+    void updateModels();
+    void handleKeyboardInput();
+    void handleMouseInput();
 };
 
 GameImpl::GameImpl(Renderer& renderer, Logger& logger)
@@ -44,7 +55,7 @@ GameImpl::GameImpl(Renderer& renderer, Logger& logger)
   m_model1Id = m_renderer.addModel(std::move(model1));
   m_model2Id = m_renderer.addModel(std::move(model2));
 
-  m_camera.translate(Vec3f(0, 0, 8));
+  m_camera.translate(Vec3f{0, 0, 8});
 }
 
 void GameImpl::onKeyDown(KeyboardKey key)
@@ -57,16 +68,58 @@ void GameImpl::onKeyUp(KeyboardKey key)
   m_keysPressed.erase(key);
 }
 
-void GameImpl::update(const Vec2f& mouseDelta)
+void GameImpl::onMouseMove(const Vec2f& delta)
 {
-  const Mat4x4f model1Translation = glm::translate(Mat4x4f(1), Vec3f(-1.5, 0, 0));
-  const Mat4x4f model2Translation = glm::translate(Mat4x4f(1), Vec3f(1.5, 0, 0));
+  m_mouseDelta = delta;
+}
+
+void GameImpl::updateModels()
+{
+  const Mat4x4f model1Translation = glm::translate(Mat4x4f(1), Vec3f{-1.5, 0, 0});
+  const Mat4x4f model2Translation = glm::translate(Mat4x4f(1), Vec3f{1.5, 0, 0});
 
   m_renderer.setModelTransform(m_model1Id, model1Translation * glm::rotate(Mat4x4f(1),
-    glm::radians<float>((90.f * m_timer.elapsed())), Vec3f(0.f, 1.f, 0.f)));
+    glm::radians<float>((90.f * m_timer.elapsed())), Vec3f{0, 1, 0}));
 
   m_renderer.setModelTransform(m_model2Id, model2Translation * glm::rotate(Mat4x4f(1),
-    glm::radians<float>((-90.f * m_timer.elapsed())), Vec3f(0.f, 1.f, 0.f)));
+    glm::radians<float>((-90.f * m_timer.elapsed())), Vec3f{0, 1, 0}));
+}
+
+void GameImpl::handleKeyboardInput()
+{
+  Vec3f dir{};
+
+  if (m_keysPressed.count(KeyboardKey::W)) {
+    dir = m_camera.getDirection();
+  }
+  if (m_keysPressed.count(KeyboardKey::S)) {
+    dir = -m_camera.getDirection();
+  }
+  if (m_keysPressed.count(KeyboardKey::D)) {
+    dir = glm::cross(m_camera.getDirection(), Vec3f{0, 1, 0});
+  }
+  if (m_keysPressed.count(KeyboardKey::A)) {
+    dir = -glm::cross(m_camera.getDirection(), Vec3f{0, 1, 0});
+  }
+
+  dir.y = 0;
+
+  if (dir != Vec3f{}) {
+    m_camera.translate(dir * PLAYER_SPEED);
+  }
+}
+
+void GameImpl::handleMouseInput()
+{
+  m_camera.rotate(-MOUSE_LOOK_SPEED * m_mouseDelta.y, -MOUSE_LOOK_SPEED * m_mouseDelta.x);
+  m_mouseDelta = Vec2f{};
+}
+
+void GameImpl::update()
+{
+  handleKeyboardInput();
+  handleMouseInput();
+  updateModels();
 
   m_renderer.update(m_camera);
 }
