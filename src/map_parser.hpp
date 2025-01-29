51cba643772319e0cc98ed6532481cdbf8809d31
Rob Jinman <jinmanr@gmail.com>
2025-01-29 20:21:36 +0000

    Add map parser and (untested) XML classes

---
+++ src/map_parser.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "math.hpp"
+#include <string>
+#include <vector>
+#include <map>
+
+struct Path
+{
+  std::vector<Vec2f> points;
+  bool closed = false;
+};
+
+using KeyValueMap = std::map<std::string, std::string>;
+
+struct ObjectData
+{
+  std::string name;
+  Path path;
+  KeyValueMap values;
+  Mat4x4f transform = identityMatrix<double, 4>();
+  std::vector<ObjectData> children;
+};
+
+Mat4x4f transformFromTriangle(const Path& path);
+
+class MapParser
+{
+  public:
+    virtual ObjectData parseMapFile(const std::string& path) const = 0;
+
+    virtual ~MapParser() {}
+};
+
+using MapParserPtr = std::unique_ptr<MapParser>;
+
+class Logger;
+
+MapParserPtr createMapParser(Logger& logger);
---
+++ src/xml.cpp
@@ -0,0 +1,134 @@
+#include "xml.hpp"
+#include "exception.hpp"
+#include "utils.hpp"
+#include <tinyxml2.h>
+#include <map>
+
+using tinyxml2::XMLDocument;
+using tinyxml2::XMLElement;
+using tinyxml2::XMLAttribute;
+using tinyxml2::XMLError;
+
+XmlNode::Iterator::Iterator(std::vector<std::unique_ptr<XmlNode>>::const_iterator i)
+  : m_i(i)
+{
+}
+
+bool XmlNode::Iterator::operator==(const Iterator& rhs) const
+{
+  return m_i == rhs.m_i;
+}
+
+bool XmlNode::Iterator::operator!=(const Iterator& rhs) const
+{
+  return !(*this == rhs);
+}
+
+const XmlNode& XmlNode::Iterator::operator*() const
+{
+  return **m_i;
+}
+
+const XmlNode* XmlNode::Iterator::operator->() const
+{
+  return m_i->get();
+}
+
+XmlNode::Iterator& XmlNode::Iterator::operator++()
+{
+  next();
+  return *this;
+}
+
+void XmlNode::Iterator::next()
+{
+  ++m_i;
+}
+
+namespace
+{
+
+class XmlNodeImpl : public XmlNode
+{
+  public:
+    XmlNodeImpl(const XMLElement& node);
+
+    const std::string& name() const override;
+    const std::string& contents() const override;
+    Iterator child(const std::string& name) const override;
+    std::string attribute(const std::string& name) const override;
+    Iterator begin() const override;
+    Iterator end() const override;
+
+  private:
+    std::string m_name;
+    std::string m_contents;
+    std::map<std::string, std::string> m_attributes;
+    std::vector<std::unique_ptr<XmlNode>> m_children;
+};
+
+XmlNodeImpl::XmlNodeImpl(const XMLElement& node)
+{
+  m_name = node.Name();
+  m_contents = node.GetText();
+
+  for (auto attr = node.FirstAttribute(); attr != nullptr; attr = attr->Next()) {
+    std::string attrName = attr->Name();
+    std::string attrValue = attr->Value();
+    m_attributes[attrName] = attrValue;
+  }
+
+  for (auto elem = node.FirstChildElement(); elem != nullptr; elem = elem->NextSiblingElement()) {
+    std::string name = elem->Name();
+    m_children.push_back(std::make_unique<XmlNodeImpl>(*elem));
+  }
+}
+
+const std::string& XmlNodeImpl::name() const
+{
+  return m_name;
+}
+
+const std::string& XmlNodeImpl::contents() const
+{
+  return m_contents;
+}
+
+XmlNode::Iterator XmlNodeImpl::child(const std::string& name) const
+{
+  for (auto i = m_children.begin(); i != m_children.end(); ++i) {
+    if ((*i)->name() == name) {
+      return i;
+    }
+  }
+
+  return end();
+}
+
+std::string XmlNodeImpl::attribute(const std::string& name) const
+{
+  auto i = m_attributes.find(name);
+  return i != m_attributes.end() ? i->second : "";
+}
+
+XmlNode::Iterator XmlNodeImpl::begin() const
+{
+  return Iterator(m_children.begin());
+}
+
+XmlNode::Iterator XmlNodeImpl::end() const
+{
+  return Iterator(m_children.end());
+}
+
+} // namespace
+
+XmlNodePtr openXmlFile(const std::string& filePath)
+{
+  XMLDocument doc;
+  if (doc.LoadFile(filePath.c_str()) != XMLError::XML_SUCCESS) {
+    EXCEPTION(STR("Error loading file " << filePath));
+  }
+
+  return std::make_unique<XmlNodeImpl>(*doc.RootElement());
+}
