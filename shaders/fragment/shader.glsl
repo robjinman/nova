49287b8a49b17f00820e9a96439a8d6204d16187
Rob Jinman <jinmanr@gmail.com>
2025-02-07 01:28:52 +0000

    Implement diffuse lighting

--- shaders/fragment/shader.glsl
+++ shaders/fragment/shader.glsl
@@ -1,6 +1,11 @@
 #version 450
 #extension GL_ARB_separate_shader_objects : enable
 
+// TODO: Pass these in
+const vec3 worldLightPos = vec3(1000, 1000, 1000);
+const vec3 lightColour = vec3(1, 1, 1);
+const float ambient = 0.1;
+
 layout(set = 1, binding = 0) uniform sampler2D texSampler;
 
 layout(push_constant) uniform Constants {
@@ -9,12 +14,19 @@ layout(push_constant) uniform Constants {
 
 layout(location = 0) in vec3 inColour;
 layout(location = 1) in vec2 inTexCoord;
+layout(location = 2) in vec3 inWorldPos;
+layout(location = 3) in vec3 inNormal;
 
 layout(location = 0) out vec4 outColour;
 
 void main() {
+  vec3 normal = normalize(inNormal);
+  vec3 lightDir = normalize(worldLightPos - inWorldPos);
+  float cosTheta = min(max(dot(normal, lightDir), 0.0), 1.0 - ambient);
+  vec3 light = ambient + cosTheta * lightColour;
+
   const vec3 white = vec3(1, 1, 1);
   vec3 texel = constants.hasTexture ? texture(texSampler, inTexCoord).rgb : white;
-  outColour = vec4(inColour * texel, 1.0);
-}
 
+  outColour = vec4(inColour * light * texel, 1.0);
+}
--- shaders/vertex/instanced.glsl
+++ shaders/vertex/instanced.glsl
@@ -1,10 +1,11 @@
 #version 450
 
 layout(set = 0, binding = 0) uniform UniformBufferObject {
-  mat4 viewProjectionMatrix;
+  mat4 viewMatrix;
+  mat4 projMatrix;
 } ubo;
 
-layout(location = 0) in vec3 inPosition;
+layout(location = 0) in vec3 inModelPos;
 layout(location = 1) in vec3 inNormal;
 layout(location = 2) in vec3 inColour;
 layout(location = 3) in vec2 inTexCoord;
@@ -14,13 +15,18 @@ layout(location = 5) in vec4 inModelMatrix1;
 layout(location = 6) in vec4 inModelMatrix2;
 layout(location = 7) in vec4 inModelMatrix3;
 
-layout(location = 0) out vec3 outFragColour;
-layout(location = 1) out vec2 outFragTexCoord;
+layout(location = 0) out vec3 outColour;
+layout(location = 1) out vec2 outTexCoord;
+layout(location = 2) out vec3 outWorldPos;
+layout(location = 3) out vec3 outNormal;
 
 void main() {
   mat4 modelMatrix = mat4(inModelMatrix0, inModelMatrix1, inModelMatrix2, inModelMatrix3);
-  gl_Position = ubo.viewProjectionMatrix * modelMatrix * vec4(inPosition, 1.0);
-  outFragColour = inColour;
-  outFragTexCoord = inTexCoord;
-}
+  vec4 worldPos = modelMatrix * vec4(inModelPos, 1.0);
+  gl_Position = ubo.projMatrix * ubo.viewMatrix * worldPos;
 
+  outColour = inColour;
+  outTexCoord = inTexCoord;
+  outWorldPos = worldPos.xyz;
+  outNormal = inNormal;
+}
