8bd89c2a4fd8652b58e3ef3d0e817f093efcaf71
Rob Jinman <jinmanr@gmail.com>
2025-02-12 18:34:38 +0000

    Refactor pipelines to remove common code

--- src/vulkan/skybox_pipeline.cpp
+++ src/vulkan/skybox_pipeline.cpp
@@ -26,8 +26,8 @@ SkyboxPipeline::SkyboxPipeline(VkDevice device, VkExtent2D swapchainExtent,
   fragShaderStageInfo.module = fragShaderModule;
   fragShaderStageInfo.pName = "main";
 
-  auto vertexBindingDescription = getDefaultVertexBindingDescription();
-  auto attributeDescriptions = getDefaultAttributeDescriptions();
+  auto vertexBindingDescription = defaultVertexBindingDescription();
+  auto attributeDescriptions = defaultAttributeDescriptions();
 
   std::vector<VkVertexInputBindingDescription> bindingDescriptions{
     vertexBindingDescription
@@ -40,75 +40,14 @@ SkyboxPipeline::SkyboxPipeline(VkDevice device, VkExtent2D swapchainExtent,
   vertexInputInfo.vertexAttributeDescriptionCount = attributeDescriptions.size();
   vertexInputInfo.pVertexAttributeDescriptions = attributeDescriptions.data();
 
-  VkPipelineInputAssemblyStateCreateInfo inputAssembly{};
-  inputAssembly.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
-  inputAssembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
-  inputAssembly.primitiveRestartEnable = VK_FALSE;
-
-  VkViewport viewport{};
-  viewport.x = 0.f;
-  viewport.y = 0.f;
-  viewport.width = swapchainExtent.width;
-  viewport.height = swapchainExtent.height;
-  viewport.minDepth = 0.f;
-  viewport.maxDepth = 1.f;
-
-  VkRect2D scissor{};
-  scissor.offset = { 0, 0 };
-  scissor.extent = swapchainExtent;
-
-  VkPipelineViewportStateCreateInfo viewportState{};
-  viewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
-  viewportState.viewportCount = 1;
-  viewportState.pViewports = &viewport;
-  viewportState.scissorCount = 1;
-  viewportState.pScissors = &scissor;
-
-  VkPipelineRasterizationStateCreateInfo rasterizer{};
-  rasterizer.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
-  rasterizer.depthClampEnable = VK_FALSE;
-  rasterizer.rasterizerDiscardEnable = VK_FALSE;
-  rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
-  rasterizer.lineWidth = 1.0f;
-  rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
-  rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
-  rasterizer.depthBiasEnable = VK_FALSE;
-  rasterizer.depthBiasConstantFactor = 0.0f;
-  rasterizer.depthBiasClamp = 0.0f;
-  rasterizer.depthBiasSlopeFactor = 0.0f;
-
-  VkPipelineMultisampleStateCreateInfo multisampling{};
-  multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
-  multisampling.sampleShadingEnable = VK_FALSE;
-  multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
-  multisampling.minSampleShading = 1.0f;
-  multisampling.pSampleMask = nullptr;
-  multisampling.alphaToCoverageEnable = VK_FALSE;
-  multisampling.alphaToOneEnable = VK_FALSE;
-
-  VkPipelineColorBlendAttachmentState colourBlendAttachment{};
-  colourBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT |
-                                         VK_COLOR_COMPONENT_G_BIT |
-                                         VK_COLOR_COMPONENT_B_BIT |
-                                         VK_COLOR_COMPONENT_A_BIT;
-  colourBlendAttachment.blendEnable = VK_FALSE;
-  colourBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE;
-  colourBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO;
-  colourBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;
-  colourBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
-  colourBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
-  colourBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;
-
-  VkPipelineColorBlendStateCreateInfo colourBlending{};
-  colourBlending.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
-  colourBlending.logicOpEnable = VK_FALSE;
-  colourBlending.logicOp = VK_LOGIC_OP_COPY;
-  colourBlending.attachmentCount = 1;
-  colourBlending.pAttachments = &colourBlendAttachment;
-  colourBlending.blendConstants[0] = 0.0f;
-  colourBlending.blendConstants[1] = 0.0f;
-  colourBlending.blendConstants[2] = 0.0f;
-  colourBlending.blendConstants[3] = 0.0f;
+  auto inputAssembly = defaultInputAssemblyState();
+  VkViewport viewport;
+  VkRect2D scissor;
+  auto viewportState = defaultViewportState(viewport, scissor, swapchainExtent);
+  auto rasterizer = defaultRasterizationState();
+  auto multisampling = defaultMultisamplingState();
+  VkPipelineColorBlendAttachmentState colourBlendAttachment;
+  auto colourBlending = defaultColourBlendState(colourBlendAttachment);
 
   VkPushConstantRange vertexPushConstants;
   vertexPushConstants.offset = 0;
@@ -141,17 +80,7 @@ SkyboxPipeline::SkyboxPipeline(VkDevice device, VkExtent2D swapchainExtent,
 
   VkPipelineShaderStageCreateInfo shaderStages[] = { vertShaderStageInfo, fragShaderStageInfo };
 
-  VkPipelineDepthStencilStateCreateInfo depthStencil{};
-  depthStencil.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
-  depthStencil.depthTestEnable = VK_TRUE;
-  depthStencil.depthWriteEnable = VK_TRUE;
-  depthStencil.depthCompareOp = VK_COMPARE_OP_LESS;
-  depthStencil.depthBoundsTestEnable = VK_FALSE;
-  depthStencil.minDepthBounds = 0.f;
-  depthStencil.maxDepthBounds = 1.f;
-  depthStencil.stencilTestEnable = VK_FALSE;
-  depthStencil.front = {};
-  depthStencil.back = {};
+  auto depthStencil = defaultDepthStencilState();
 
   VkGraphicsPipelineCreateInfo pipelineInfo{};
   pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
