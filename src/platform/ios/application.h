3dbe0e44162fb04688daa7c5ee5318bda6a5be7e
Rob Jinman <jinmanr@gmail.com>
2025-03-02 11:27:16 +0000

    iOS build implemented, but displaying black screen

---
+++ src/platform/ios/application.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "window_delegate.hpp"
+#include <memory>
+
+class Application
+{
+  public:
+    virtual void update() = 0;
+
+    virtual ~Application() {}
+};
+
+using ApplicationPtr = std::unique_ptr<Application>;
+
+ApplicationPtr createApplication(WindowDelegatePtr windowDelegate);
