ae7bb4290b30807876b806a414dec1311a994958
Rob Jinman <jinmanr@gmail.com>
2025-03-08 21:20:44 +0000

    Remove java from android build and add data files

--- nova/src/platform/android/src/main/cpp/app.cpp
+++ nova/src/platform/android/src/main/cpp/app.cpp
@@ -1,10 +1,26 @@
-#include <jni.h>
-#include <string>
+#include <android/asset_manager.h>
+#include <android_native_app_glue.h>
+#include <android/log.h>
+#include <cstdlib>
+#include <cstring>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_nativeexample_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+void android_main(android_app* app)
+{
+  AAssetManager* assetManager = app->activity->assetManager;
+  AAsset* asset = AAssetManager_open(assetManager, "map.svg", AASSET_MODE_BUFFER);
+  size_t assetLength = AAsset_getLength(asset);
+  char* buffer = (char*)malloc(assetLength + 1);
+  memset(buffer, '\0', assetLength + 1);
+  AAsset_read(asset, buffer, assetLength);
+
+  __android_log_print(ANDROID_LOG_ERROR, "eggplant", "Data file: %s", buffer);
+
+  while (!app->destroyRequested) {
+    android_poll_source* source = nullptr;
+    ALooper_pollOnce(-1, nullptr, nullptr, reinterpret_cast<void**>(&source));
+
+    if (source != nullptr) {
+      source->process(app, source);
+    }
+  }
 }
