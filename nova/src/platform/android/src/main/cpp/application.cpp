4fab706fe1cb14f67f11df8f5c97c95b699268a1
Rob Jinman <jinmanr@gmail.com>
2025-03-16 14:35:36 +0000

    Android controller support

--- nova/src/platform/android/src/main/cpp/application.cpp
+++ nova/src/platform/android/src/main/cpp/application.cpp
@@ -18,8 +18,6 @@
 #include <android/log.h>
 #include <iostream>
 
-const char* LOG_TAG = "eggplant";
-
 LoggerPtr createAndroidLogger();
 WindowDelegatePtr createWindowDelegate(ANativeWindow& window);
 FileSystemPtr createAndroidFileSystem(AAssetManager& assetManager);
@@ -27,18 +25,36 @@ FileSystemPtr createAndroidFileSystem(AAssetManager& assetManager);
 namespace
 {
 
+KeyboardKey keyCode(int32_t key)
+{
+  if (key >= AKEYCODE_A && key <= AKEYCODE_Z) {
+    auto i = static_cast<std::underlying_type_t<KeyboardKey>>(KeyboardKey::A) + (key - AKEYCODE_A);
+    return static_cast<KeyboardKey>(i);
+  }
+
+  switch (key) {
+    case AKEYCODE_SPACE: return KeyboardKey::Space;
+    case AKEYCODE_BUTTON_A: return KeyboardKey::E;  // TODO: Do button mapping inside Game class
+    default: return KeyboardKey::Z; // TODO
+  }
+}
+
 class Application
 {
   public:
-    Application(WindowDelegatePtr windowDelegate, FileSystemPtr fileSystem);
+    Application(WindowDelegatePtr windowDelegate, FileSystemPtr fileSystem, Logger& logger);
 
     void update();
     void onConfigChange();
+    void onLeftStickMove(float_t x, float_t y);
+    void onRightStickMove(float_t x, float_t y);
+    void onKeyDown(KeyboardKey key);
+    void onKeyUp(KeyboardKey key);
 
   private:
+    Logger& m_logger;
     WindowDelegatePtr m_windowDelegate;
     FileSystemPtr m_fileSystem;
-    LoggerPtr m_logger;
     RendererPtr m_renderer;
     SpatialSystemPtr m_spatialSystem;
     RenderSystemPtr m_renderSystem;
@@ -46,32 +62,36 @@ class Application
     MapParserPtr m_mapParser;
     EntityFactoryPtr m_entityFactory;
     GamePtr m_game;
+    Vec2f m_leftStickDelta;
+    Vec2f m_rightStickDelta;
 };
 
 using ApplicationPtr = std::unique_ptr<Application>;
 
-Application::Application(WindowDelegatePtr windowDelegate, FileSystemPtr fileSystem)
-  : m_windowDelegate(std::move(windowDelegate))
+Application::Application(WindowDelegatePtr windowDelegate, FileSystemPtr fileSystem, Logger& logger)
+  : m_logger(logger)
+  , m_windowDelegate(std::move(windowDelegate))
   , m_fileSystem(std::move(fileSystem))
 {
-  m_logger = createAndroidLogger();
-  m_renderer = createRenderer(*m_fileSystem, *m_windowDelegate, *m_logger);
-  m_spatialSystem = createSpatialSystem(*m_logger);
-  m_renderSystem = createRenderSystem(*m_spatialSystem, *m_renderer, *m_logger);
-  m_collisionSystem = createCollisionSystem(*m_spatialSystem, *m_logger);
-  m_mapParser = createMapParser(*m_fileSystem, *m_logger);
+  m_renderer = createRenderer(*m_fileSystem, *m_windowDelegate, m_logger);
+  m_spatialSystem = createSpatialSystem(m_logger);
+  m_renderSystem = createRenderSystem(*m_spatialSystem, *m_renderer, m_logger);
+  m_collisionSystem = createCollisionSystem(*m_spatialSystem, m_logger);
+  m_mapParser = createMapParser(*m_fileSystem, m_logger);
   m_entityFactory = createEntityFactory(*m_spatialSystem, *m_renderSystem, *m_collisionSystem,
-    *m_fileSystem, *m_logger);
+    *m_fileSystem, m_logger);
 
   auto player = createScene(*m_entityFactory, *m_spatialSystem, *m_renderSystem, *m_collisionSystem,
-    *m_mapParser, *m_fileSystem, *m_logger);
+    *m_mapParser, *m_fileSystem, m_logger);
 
   m_renderSystem->start();
-  m_game = createGame(std::move(player), *m_collisionSystem, *m_logger);
+  m_game = createGame(std::move(player), *m_collisionSystem, m_logger);
 }
 
 void Application::update()
 {
+  m_game->onLeftStickMove(m_leftStickDelta);
+  m_game->onRightStickMove(m_rightStickDelta);
   m_game->update();
   m_spatialSystem->update();
   m_renderSystem->update();
@@ -80,20 +100,128 @@ void Application::update()
 
 void Application::onConfigChange()
 {
-  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "Resizing window");
   m_renderer->onResize();
 }
 
-ApplicationPtr createApplication(android_app& state)
+void Application::onLeftStickMove(float_t x, float_t y)
+{
+  m_leftStickDelta = { x, y };
+}
+
+void Application::onRightStickMove(float_t x, float_t y)
+{
+  m_rightStickDelta = { x, y };
+}
+
+void Application::onKeyDown(KeyboardKey key)
+{
+  m_game->onKeyDown(key);
+}
+
+void Application::onKeyUp(KeyboardKey key)
+{
+  m_game->onKeyUp(key);
+}
+
+ApplicationPtr createApplication(android_app& state, Logger& logger)
 {
   ASSERT(state.window != nullptr, "Window is NULL");
 
   auto windowDelegate = createWindowDelegate(*state.window);
   FileSystemPtr fileSystem = createAndroidFileSystem(*state.activity->assetManager);
-  return std::make_unique<Application>(std::move(windowDelegate), std::move(fileSystem));
+  return std::make_unique<Application>(std::move(windowDelegate), std::move(fileSystem), logger);
 }
 
-bool waitForWindow(android_app& state)
+class EventHandler
+{
+  public:
+    EventHandler(Logger& logger);
+
+    void setApplication(Application* app);
+
+    void onCommandEvent(int32_t command);
+    int32_t onInputEvent(const AInputEvent& event);
+
+    bool isWindowInitialised() const;
+
+  private:
+    Logger& m_logger;
+    Application* m_app = nullptr;
+    bool m_windowInitialised = false;
+};
+
+EventHandler::EventHandler(Logger& logger)
+  : m_logger(logger)
+{
+}
+
+void EventHandler::setApplication(Application* app)
+{
+  m_app = app;
+}
+
+void EventHandler::onCommandEvent(int32_t command)
+{
+  if (command == APP_CMD_INIT_WINDOW) {
+    m_windowInitialised = true;
+    return;
+  }
+
+  if (m_app) {
+    switch (command) {
+      case APP_CMD_CONFIG_CHANGED:
+        m_app->onConfigChange();
+        break;
+      default: break;
+    }
+  }
+}
+
+int32_t EventHandler::onInputEvent(const AInputEvent& event)
+{
+  if (m_app) {
+    int32_t eventType = AInputEvent_getType(&event);
+    int32_t deviceId = AInputEvent_getDeviceId(&event);
+
+    if (eventType == AINPUT_EVENT_TYPE_KEY) {
+      int32_t action = AKeyEvent_getAction(&event);
+      int32_t key = AKeyEvent_getKeyCode(&event);
+
+      if (action == AKEY_EVENT_ACTION_DOWN) {
+        m_app->onKeyDown(keyCode(key));
+      }
+      else if (action == AKEY_EVENT_ACTION_UP) {
+        m_app->onKeyUp(keyCode(key));
+      }
+
+      return 1;
+    }
+    if (eventType == AINPUT_EVENT_TYPE_MOTION) {
+      if (AInputEvent_getSource(&event) == AINPUT_SOURCE_JOYSTICK) {
+        float x = AMotionEvent_getAxisValue(&event, AMOTION_EVENT_AXIS_X, 0);
+        float y = AMotionEvent_getAxisValue(&event, AMOTION_EVENT_AXIS_Y, 0);
+
+        m_app->onLeftStickMove(x, y);
+
+        float z = AMotionEvent_getAxisValue(&event, AMOTION_EVENT_AXIS_Z, 0);
+        float rz = AMotionEvent_getAxisValue(&event, AMOTION_EVENT_AXIS_RZ, 0);
+
+        m_app->onRightStickMove(z, rz);
+      }
+
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
+bool EventHandler::isWindowInitialised() const
+{
+  return m_windowInitialised;
+}
+
+bool waitForWindow(android_app& state, EventHandler& handler)
 {
   FrameRateLimiter frameRateLimiter{TARGET_FRAME_RATE};
 
@@ -105,7 +233,7 @@ bool waitForWindow(android_app& state)
       source->process(&state, source);
     }
 
-    if (state.userData) {
+    if (handler.isWindowInitialised()) {
       return true;
     }
 
@@ -115,41 +243,35 @@ bool waitForWindow(android_app& state)
   return false;
 }
 
-enum class StateChange : size_t
-{
-  WindowInitialised = 1,
-  ConfigChanged
-};
-
 void handleCommand(android_app* state, int32_t command)
 {
-  switch (command) {
-    case APP_CMD_INIT_WINDOW:
-      state->userData = reinterpret_cast<void*>(StateChange::WindowInitialised);
-      __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "Got event! Window initialised");
-      break;
-    case APP_CMD_CONFIG_CHANGED:
-      state->userData = reinterpret_cast<void*>(StateChange::ConfigChanged);
-      __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "Got event! Config changed");
-      break;
-    default: break;
-  }
+  reinterpret_cast<EventHandler*>(state->userData)->onCommandEvent(command);
+}
+
+int32_t handleInput(android_app* state, AInputEvent* event)
+{
+  return reinterpret_cast<EventHandler*>(state->userData)->onInputEvent(*event);
 }
 
 } // namespace
 
 void android_main(android_app* state)
 {
-  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "Starting Nova");
+  auto logger = createAndroidLogger();
+  logger->info("Starting Nova");
+
+  EventHandler handler{*logger};
 
   state->onAppCmd = handleCommand;
-  state->userData = nullptr;
+  state->onInputEvent = handleInput;
+  state->userData = &handler;
 
-  if (!waitForWindow(*state)) {
+  if (!waitForWindow(*state, handler)) {
     return;
   }
 
-  auto application = createApplication(*state);
+  auto application = createApplication(*state, *logger);
+  handler.setApplication(application.get());
 
   FrameRateLimiter frameRateLimiter{TARGET_FRAME_RATE};
 
@@ -161,11 +283,6 @@ void android_main(android_app* state)
       source->process(state, source);
     }
 
-    if (reinterpret_cast<size_t>(state->userData) ==
-      static_cast<size_t>(StateChange::ConfigChanged)) {
-
-      application->onConfigChange();
-    }
     application->update();
 
     frameRateLimiter.wait();
