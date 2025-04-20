872b2f0f3eb238ca89c4abdfbf4dbb80ed85c19a
Rob Jinman <jinmanr@gmail.com>
2025-04-20 17:47:33 +0100

    Better frame rate control

--- nova/src/time.cpp
+++ nova/src/time.cpp
@@ -1,7 +1,17 @@
 #include "time.hpp"
 #include <thread>
 
+#ifdef _WIN32
+#include <windows.h>
+static bool init = []() {
+  timeBeginPeriod(1);
+  return true;
+}();
+#endif
+
 using namespace std::chrono_literals;
+
+const double SLEEP_RATIO = 0.9;
 
 FrameRateLimiter::FrameRateLimiter(unsigned frameRate)
   : m_frameDuration(1000000us / frameRate)
@@ -10,11 +20,20 @@ FrameRateLimiter::FrameRateLimiter(unsigned frameRate)
 
 void FrameRateLimiter::wait()
 {
-  auto time = std::chrono::high_resolution_clock::now();
-  auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(time - m_lastFrameTime);
+  auto getElapsed = [this]() {
+    auto time = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration_cast<std::chrono::microseconds>(time - m_lastFrameTime);
+  };
 
+  auto busyWait = [&]() {
+    while (getElapsed() < m_frameDuration) {}
+  };
+
+  auto elapsed = getElapsed();
   if (elapsed < m_frameDuration) {
-    std::this_thread::sleep_for(m_frameDuration - elapsed);
+    std::this_thread::sleep_for((m_frameDuration - elapsed) * SLEEP_RATIO);
+
+    busyWait();
   }
 
   m_lastFrameTime = std::chrono::high_resolution_clock::now();
